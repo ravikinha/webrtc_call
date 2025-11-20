ef92d0d433e56e010f7997d3477d59b94a455ac4
ravi <ravi.kumar@cyberpointmedia.com>
2025-11-20 14:34:30 +0530

    Add iOS project files

---
+++ iOS/webrtc_call_ios/webrtc_call_ios/webrtc_call_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  webrtc_call_iosApp.swift
+//  webrtc_call_ios
+//
+//  Created by Ravi on 20/11/25.
+//
+
+import SwiftUI
+
+@main
+struct webrtc_call_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
